aef5f464b05c6e8165a4722cc41470baa08252a7
Nikola <nikola.sem450@gmail.com>
2022-09-07 18:09:13 +0200

    Updated server starting point

--- server.js
+++ server.js
@@ -1,38 +1,36 @@
 const express = require('express');
-const fs = require("fs");
 const bodyParser = require('body-parser');
 const cors = require('cors');
+
+require('./database/prepareDB');
+
+const wsQuery = require('./queries/workspace_query');
+const viewQuery = require('./queries/view_query');
+
 const PORT = process.env.PORT || 3000;
 
 const app = express();
 app.use(bodyParser.json());
+app.use(
+   bodyParser.urlencoded({
+     extended: true
+   })
+ );
 app.use(cors());
 
-app.get('/', (req, res) => {
-   res.send('Hello World!')
+app.get('/', (_req, res) => {
+   res.send('Node.js, Express, and Postgres API')
 });
 
-app.get('/workspace', function (req, res) {
-   // First read existing users.
-   fs.readFile(__dirname + "/" + "workspace.json", 'utf8', function (err, data) {
-      const workspace = JSON.parse(data);
-      console.log(workspace);
-      res.end(JSON.stringify(workspace));
-   });
-});
+app.post('/workspace', wsQuery.createWorkspace);
+app.get('/workspace', wsQuery.readWorkspaceList);
+app.get('/workspace/:id', wsQuery.readWorspaceByID);
+app.delete('/worspace/:id', wsQuery.deleteWorkspace);
 
-app.get('/view', function (req, res) {
-   const idNotParsed = req.query.id;
-   const id = parseInt(req.query.id);
-   //  console.log("PARAM ID >> ", id);
-   //  console.log("PARAM ID >> ", idNotParsed, " - TYPE: ", typeof idNotParsed);
-   // First read existing users.
-   fs.readFile(__dirname + "/" + "view.json", 'utf8', function (err, data) {
-      const vew = JSON.parse(data);
-      res.end(JSON.stringify(vew));
-   });
-});
+app.post('/view', viewQuery.createView);
+app.get('/view/:id', viewQuery.readViewByID);
+app.delete('/view/:id', viewQuery.deleteView);
 
 app.listen(PORT, () => {
-   console.log(`Our app is running on port ${PORT}`);
+   console.log(`App is running on port ${PORT}`);
 });
