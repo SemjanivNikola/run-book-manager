ddad013545efe8a83c65fa1c4fbf3438e50ab3ad
Nikola <nikola.sem450@gmail.com>
2022-09-08 14:21:43 +0200

    Update starting point with using middleware

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 const express = require('express');
+const fs = require("fs");
 const bodyParser = require('body-parser');
 const cors = require('cors');
 
 require('./database/prepareDB');
 
-const wsQuery = require('./queries/workspace_query');
-const viewQuery = require('./queries/view_query');
+const workspaceHandler = require('./middleware/workspaceHandler');
+
+const wsQuery = require('./queries/workspaceQuery');
+const viewQuery = require('./queries/viewQuery');
 
 const PORT = process.env.PORT || 3000;
 
@@ -22,7 +25,7 @@ app.get('/', (_req, res) => {
    res.send('Node.js, Express, and Postgres API')
 });
 
-app.post('/workspace', wsQuery.createWorkspace);
+app.post('/workspace', workspaceHandler.create);
 app.get('/workspace', wsQuery.readWorkspaceList);
 app.get('/workspace/:id', wsQuery.readWorspaceByID);
 app.delete('/worspace/:id', wsQuery.deleteWorkspace);
@@ -30,6 +33,18 @@ app.delete('/worspace/:id', wsQuery.deleteWorkspace);
 app.post('/view', viewQuery.createView);
 app.get('/view/:id', viewQuery.readViewByID);
 app.delete('/view/:id', viewQuery.deleteView);
+
+app.get('/viewtest', function (req, res) {
+   const idNotParsed = req.query.id;
+   const id = parseInt(req.query.id);
+   //  console.log("PARAM ID >> ", id);
+   //  console.log("PARAM ID >> ", idNotParsed, " - TYPE: ", typeof idNotParsed);
+   // First read existing users.
+   fs.readFile(__dirname + "/" + "view.json", 'utf8', function (err, data) {
+      const vew = JSON.parse(data);
+      res.end(JSON.stringify(vew));
+   });
+});
 
 app.listen(PORT, () => {
    console.log(`App is running on port ${PORT}`);
